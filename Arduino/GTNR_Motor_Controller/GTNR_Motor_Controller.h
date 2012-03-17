ffea54db4e0650b5e2d6b08991c505ca39d1c976
desposi1 <desposi1@14e5fdfe-5839-473e-a997-266854a79fde>
2012-03-17 22:36:57 +0000

    Started motor controller sketch. Broke most functionality into libraries. TODO: implement sensor functions.

---
+++ Arduino/GTNR_Motor_Controller/GTNR_Motor_Controller.h
@@ -0,0 +1,65 @@
+#ifndef GTNR_Motor_Controller_h
+#define GTNR_Motor_Controller_h
+
+/**
+ * ===========================================================
+ *  Custom Keywords
+ * ===========================================================
+ */
+#define FWD  0
+#define BKD  1
+#define RHT  2
+#define LFT  3
+#define AUT  4
+#define STOP 5
+
+
+
+
+/**
+ * ===========================================================
+ *  DFRduino Motor Variables
+ * ===========================================================
+ */
+
+//Standard PWM DC control
+int E1 = 5;     //M1 Speed Control
+int E2 = 6;     //M2 Speed Control
+int M1 = 4;    //M1 Direction Control
+int M2 = 7;    //M1 Direction Control
+
+
+/**
+ * ===========================================================
+ *  Other Variables
+ * ===========================================================
+ */
+int count = 0;
+int currSpeed = 0;
+int currDirection = 0;
+int autopilot = 1;
+int MAX_SPEED = 255;
+
+/**
+ * ===========================================================
+ *  Motor Controller Methods
+ * ===========================================================
+ */
+void handle_key();
+void go(int dir);
+void halt(void);
+void advance(char a,char b);
+void back_off (char a,char b);
+void turn_L (char a,char b);
+void turn_R (char a,char b);
+
+/**
+ * ===========================================================
+ *  Sensor Methods
+ * ===========================================================
+ */
+void handle_encoder(int side);
+int readIR(int side);
+
+
+#endif

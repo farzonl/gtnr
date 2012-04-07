06e620950f376152196ba1cfa0c0fad1e735a647
fonzi2006@gmail.com <fonzi2006@gmail.com@14e5fdfe-5839-473e-a997-266854a79fde>
2012-04-07 23:17:42 +0000

    I changed up a bit of the graph functionality, not noticable change. Also added robust multithreaded network protoccol to communicate with and monitor the atom board

--- GTNR_Java/src/com/gtnightrover/Graph/Edge.java
+++ GTNR_Java/src/com/gtnightrover/Graph/Edge.java
@@ -1,48 +1,114 @@
 package com.gtnightrover.Graph;
 
-public class Edge {
-	
+/*
+ * Written by Farzon Lotfi
+ * 
+ * Edge.java
+ * */
+
+/**
+ * @author Farzon Lotfi
+ * @version 1.0
+ *a class that lets you encapsulate the data regarding the edge
+ */
+public class Edge implements Comparable{
+
 	private int distance;
-	
+
 	private String edgeName;
-	
+
 	private Vertex destination;
 	
-	public Edge(int distance, String edgeName,Vertex destination) {
+	//private Vertex origin;
+
+	/**
+	 *constructor that encapsulates the data regarding the edge
+	 *@param int distance - the length of the edge
+	 *@param String edgeName - the name of the edge
+	 *@param Vertex destination - the destination of the edge
+	 */
+	//public Edge(int distance, String edgeName,Vertex origin, Vertex destination) {
+	public Edge(int distance, String edgeName, Vertex destination) {
 		this.setDestination(destination);
+		//this.setOrigin(origin);
 		this.distance = distance;
 		this.edgeName = edgeName;
 	}
 
+	/**
+	 *a getter for the distance
+	 *@return int distance - length of the edge
+	 */
 	public int getDistance() {
 		return distance;
 	}
 
+
+	/**
+	 *a setter for the distance
+	 *@param int distance - length of the edge
+	 */
 	public void setDistance(int distance) {
 		this.distance = distance;
 	}
 
+	/**
+	 *a getter for the edgeName
+	 *@return String edgeName - the name of the edge
+	 */
 	public String getEdgeName() {
 		return edgeName;
 	}
 
+	/**
+	 *a setter for the edgeName
+	 *@param String edgeName - the name of the edge
+	 */
 	public void setEdgeName(String edgeName) {
 		this.edgeName = edgeName;
 	}
 
 	/**
-	 * @param destination the destination to set
+	 *a setter for the destination
+	 *@param Vertex destination - vertex the edge is headed towards
 	 */
 	public void setDestination(Vertex destination) {
 		this.destination = destination;
 	}
-	
-	
+
+
 	/**
-	 * @return the destination
+	 *a getter for the destination
+	 *@return Vertex destination - vertex the edge is headed towards
 	 */
 	public Vertex getDestination() {
 		return destination;
 	}	
-	
+
+	/**
+	 *the method that makes the edges comparable so that they can be used
+	 *in the priorityQueue
+	 *@param Object arg0- the edge you want compared
+	 *@return int - lets the priorityQueue know where to put the value
+	 */
+	public int compareTo(Object arg0) {
+		Edge val =(Edge)(arg0);
+		return this.getDistance() - val.getDistance();
+	}
+
+	/**
+	 * @param origin the origin to set
+	 */
+/*	public void setOrigin(Vertex origin) {
+		this.origin = origin;
+	}*/
+
+	/**
+	 * @return the origin
+	 */
+/*	public Vertex getOrigin() {
+		return origin;
+	}*/
+
+
 }

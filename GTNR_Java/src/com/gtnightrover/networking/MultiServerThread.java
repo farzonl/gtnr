ef395e447d54b6f45e78d77299c4e345010fa6a3
fonzi2006@gmail.com <fonzi2006@gmail.com@14e5fdfe-5839-473e-a997-266854a79fde>
2012-04-10 18:41:45 +0000

    lidar integration  not tested

--- GTNR_Java/src/com/gtnightrover/networking/MultiServerThread.java
+++ GTNR_Java/src/com/gtnightrover/networking/MultiServerThread.java
@@ -39,10 +39,15 @@ public class MultiServerThread extends Thread
 					ObjectOutputStream outStream =
 							new  ObjectOutputStream(socket.getOutputStream());
 					//System.out.println(Arrays.toString(MultiServer.depth_arr));
-
+					ArrayList<Integer> sendList = null;
+					if(MultiServer.isSerial)
+						sendList = MultiServer.sendDepth(MultiServer.stream.getDistances());
+					else
+						sendList = MultiServer.sendDepth();
+						
 					while(true)
 					{
-						if(MultiServer.send_depth_arr != null && MultiServer.send_depth_arr.size() == 360) 
+						if(sendList != null && sendList.size() == 360) 
 						{
 							System.out.println(Arrays.toString(MultiServer.send_depth_arr.toArray()));
 							outStream.writeObject(MultiServer.send_depth_arr);

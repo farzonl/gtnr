9c2ff46c9a77d080b2bc9eecbe36c3c49396fe9b
desposi1 <desposi1@14e5fdfe-5839-473e-a997-266854a79fde>
2012-04-17 07:04:10 +0000

    Check comments in Main.java. Functional tested code. Need multimeter to make further progress.

--- trunk/GTNR_Java/src/com/gtnightrover/Main.java
+++ trunk/GTNR_Java/src/com/gtnightrover/Main.java
@@ -9,20 +9,44 @@ import com.gtnightrover.visualizer.StreamManager;
 public class Main {
 
 	/**
+	 * To setup the hardware for this program:
+	 * 
+	 *  1. Double check connections:
+	 *  	a) Lidar power to 5V and GND on Mega Board, and lidar data
+	 *     		to RX1 pin.
+	 *      b) Lidar motor red to M1+, black to M1-.
+	 *      c) Power from power supply to motor terminal on DFRduino (M_VIN, GND)
+	 *      d) Both Arduino boards are connected with USB.
+	 * 	1. Load the 'LidarDriverTest' sketch onto a DFRduino using
+	 * 		'Arduino Duemilanove w/ ATmega328 on /dev/USB#'
+	 *  2. Turn on power supply to 11V. Make sure the 'Output On/Off' 
+	 *    	button is lit.
+	 *  3. Turn on lidar power switch (on 5V line from mega).
+	 *  4. Start the GUI and note speed printing to console. Speed should settle
+	 *    	to 0x4d## in less than 30 seconds. If speeds is greater than
+	 *    	0x50 after setup time something is wrong.
+	 * 
 	 * @param args
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
+		// Set up GUI
 		Graph g = new Graph(800,600,-12000,-12000,12000,12000);
 		GraphWindow f = new GraphWindow(800, 600, g);
 		f.setVisible(true);
-//		new StreamManager(g, f, new StreamGen()).start();
-//		new StreamManager(g, f, new ArdionoSerialStream("/dev/ttyACM0", 115200)).start();
-		LidarSerialStream lss = new LidarSerialStream("/dev/ttyACM0", 115200);
-		lss.start();
+		
+		// Declare and initialize serial communication with lidar driver. This
+		// is required to optimize lidar speed.
 		LidarComm dfr = new LidarComm("/dev/ttyUSB0", 9600);
-		System.out.println("Lidar Serial Stream Started");
-		new StreamManager(g, f, lss, dfr).start();
+		
+		// Declare and initialize serial communication with lidar module.
+		// Note the LidarComm object being passed in. It is optional, but required
+		// for speed control.
+		LidarSerialStream lss = new LidarSerialStream("/dev/ttyACM0", 115200, dfr);
+		lss.start();
+		
+		// Create the thread which will automatically update the GUI with lidar data
+		new StreamManager(g, f, lss).start();
 	}
 
 }
